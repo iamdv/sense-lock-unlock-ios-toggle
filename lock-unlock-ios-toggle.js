0e85d54fda16d6333a237026539c2541b93d57f1
DV <me@iamdv.io>
2016-08-30 11:56:35 +0100

    Update lock-unlock-ios-toggle.js
    Added custom label along with <b> and <i> formatting

--- lock-unlock-ios-toggle.js
+++ lock-unlock-ios-toggle.js
@@ -11,13 +11,60 @@ define( [
 function (qlik, $, cssContent) {
 
 var app = qlik.currApp(this);
-		
+
+var myCustomObj = {
+	myObjTextBox: {
+		ref:"props.myCustomObj.myObjTextBox",
+		label:"ios Toggle Label",
+		type:"string",
+	},
+	myObjLabelBold:{
+		component:"checkbox",
+		ref:"props.myCustomObj.myObjLabelBold",
+		label:"Bold",
+		type:"boolean"
+	},
+	myObjLabelItalic:{
+		component:"checkbox",
+		ref:"props.myCustomObj.myObjLabelItalic",
+		label:"Italic",
+		type:"boolean"
+	}	
+};
+
+var myAppearenceSection = {
+	uses:"settings",
+	items: {
+		myListItems: {
+			label: "Variable Properties",
+			items: {
+					myObjTextBox: myCustomObj.myObjTextBox,
+					myObjLabelBold: myCustomObj.myObjLabelBold,
+					myObjLabelItalic: myCustomObj.myObjLabelItalic
+			}			
+		}		
+	}
+};
 
 	return {
-		paint: function ($element) {
+		definition:{
+			type: "items",
+			component:"accordion",
+			items:{
+				appearance: myAppearenceSection
+			}
+		},
+		paint: function ($element, layout) {
 
             var myDevMessage = '############# Hi there, this extension has been built by Deepak Vadithala. Follow me @dvadithala #############';
-            var myHTML = '<div class="item"> <p>Lock Current Selections</p> <input type="checkbox" id="id_lock_unlock_toggle" name="" value=""> <div class="toggle"> <label for="id_lock_unlock_toggle"><i></i></label> </div> </div>'
+            var myHTML = '<div class="item"> <p>' 
+            + (layout.props.myCustomObj.myObjLabelBold ? '<b>' : '' ) 
+            + (layout.props.myCustomObj.myObjLabelItalic ? '<i>' : '' ) 
+            +  (typeof layout.props.myCustomObj.myObjTextBox == "undefined" ? '' : layout.props.myCustomObj.myObjTextBox)  
+            + (layout.props.myCustomObj.myObjLabelItalic ? '</i>' : '' )
+            + (layout.props.myCustomObj.myObjLabelBold ? '</b>' : '' ) 
+            + '</p> <input type="checkbox" id="id_lock_unlock_toggle" name="" value=""> <div class="toggle"> <label for="id_lock_unlock_toggle"><i></i></label> </div> </div>'
+			
 			var myValueToggle = 0;
 
             $element.html(myHTML);
