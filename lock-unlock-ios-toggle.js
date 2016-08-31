2352fd1034e1830d1f4d5ae9d63d6ae5153e6c43
DV <me@iamdv.io>
2016-08-31 17:37:33 +0100

    Update lock-unlock-ios-toggle.js

--- lock-unlock-ios-toggle.js
+++ lock-unlock-ios-toggle.js
@@ -5,7 +5,7 @@ define( [
     // "./js/myCode"
 ],
 /**
-* @owner Deepak Vadithala (AKA: DV), @dvadithala
+* @owner Deepak Vadithala (AKA: DV), @dvadithala, GitHub: iamdv
 */
 
 function (qlik, $, cssContent) {
@@ -13,8 +13,24 @@ function (qlik, $, cssContent) {
 var app = qlik.currApp(this);
 
 var myCustomObj = {
-	myObjTextBox: {
-		ref:"props.myCustomObj.myObjTextBox",
+	myObjLockAllOrFewFields: {
+		type: "boolean",
+		component: "radiobuttons",
+		label: "Select One: ",
+		ref: "props.myCustomObj.myObjLockAllOrFewFields",
+		options: [{value: 1, label: "Lock/Unlock All Fields"}, {value: 2, label: "Lock/Unlock by User Selections"}],
+		defaultValue: 2 
+	},		
+	myObjSwitchIncludeClearField: {
+		type: "boolean",
+		component: "switch",
+		label: "Enable Clear Field Selections",
+		ref: "props.myCustomObj.myObjSwitchIncludeClearField",
+		options: [{value: true, label: "Enabled"}, {value: false, label: "Not Enabled"}],
+		defaultValue: false
+	},	
+	myObjLabel: {
+		ref:"props.myCustomObj.myObjLabel",
 		label:"ios Toggle Label",
 		type:"string",
 	},
@@ -29,7 +45,16 @@ var myCustomObj = {
 		ref:"props.myCustomObj.myObjLabelItalic",
 		label:"Italic",
 		type:"boolean"
-	}	
+	},
+	myObjLockOptionsText: {
+		component: "text",
+		label:"Specify Field Manager Name only if you have opted - 'Lock/Unlock by User Selections'"
+	},	
+	myObjLockFieldName: {
+		ref:"props.myCustomObj.myObjLockFieldName",
+		label:"Data Island Name (Ex: $Field)",
+		type:"string",
+	}
 };
 
 var myAppearenceSection = {
@@ -38,9 +63,13 @@ var myAppearenceSection = {
 		myListItems: {
 			label: "Variable Properties",
 			items: {
-					myObjTextBox: myCustomObj.myObjTextBox,
+					myObjLockAllOrFewFields: myCustomObj.myObjLockAllOrFewFields,
+					myObjSwitchIncludeClearField: myCustomObj.myObjSwitchIncludeClearField,
+					myObjLabel: myCustomObj.myObjLabel,
 					myObjLabelBold: myCustomObj.myObjLabelBold,
-					myObjLabelItalic: myCustomObj.myObjLabelItalic
+					myObjLabelItalic: myCustomObj.myObjLabelItalic,
+					myObjLockOptionsText: myCustomObj.myObjLockOptionsText,
+					myObjLockFieldName: myCustomObj.myObjLockFieldName
 			}			
 		}		
 	}
@@ -56,31 +85,103 @@ var myAppearenceSection = {
 		},
 		paint: function ($element, layout) {
 
-            var myDevMessage = '############# Hi there, this extension has been built by Deepak Vadithala. Follow me @dvadithala #############';
-            var myHTML = '<div class="item"> <p>' 
-            + (layout.props.myCustomObj.myObjLabelBold ? '<b>' : '' ) 
-            + (layout.props.myCustomObj.myObjLabelItalic ? '<i>' : '' ) 
-            +  (typeof layout.props.myCustomObj.myObjTextBox == "undefined" ? '' : layout.props.myCustomObj.myObjTextBox)  
-            + (layout.props.myCustomObj.myObjLabelItalic ? '</i>' : '' )
-            + (layout.props.myCustomObj.myObjLabelBold ? '</b>' : '' ) 
-            + '</p> <input type="checkbox" id="id_lock_unlock_toggle" name="" value=""> <div class="toggle"> <label for="id_lock_unlock_toggle"><i></i></label> </div> </div>'
-			
-			var myValueToggle = 0;
+			/* ==========================================================*/
+			/* Creating custom object which holds the configuration
+			settings */
 
+			var myObjLockUnlockProps ={
+				option: layout.props.myCustomObj.myObjLockAllOrFewFields,
+				clearfield: layout.props.myCustomObj.myObjSwitchIncludeClearField,
+				label: layout.props.myCustomObj.myObjLabel,
+				bold: layout.props.myCustomObj.myObjLabelBold,
+				italic: layout.props.myCustomObj.myObjLabelItalic,
+				fieldname: layout.props.myCustomObj.myObjLockFieldName
+			};
+
+			/* ==========================================================*/			
+			
+			/* ==========================================================*/
+			/* This block of code generate relevant HTML and renders  */
+
+            var myDevMessage = '############# Hi there, this extension was developed by Deepak Vadithala. Follow me @dvadithala #############';
+            var myHTML = '<div class="item"> <p>' 
+	            + (myObjLockUnlockProps.bold ? '<b>' : '' ) 
+	            + (myObjLockUnlockProps.italic ? '<i>' : '' ) 
+	            +  (typeof myObjLockUnlockProps.label == "undefined" ? '' : myObjLockUnlockProps.label)  
+	            + (myObjLockUnlockProps.italic ? '</i>' : '' )
+	            + (myObjLockUnlockProps.bold ? '</b>' : '' ) 
+	            + '</p> <input type="checkbox" id="id_lock_unlock_toggle" name="" value=""> <div class="toggle"> <label for="id_lock_unlock_toggle"><i></i></label> </div> </div>';
+			
             $element.html(myHTML);
             console.log(myDevMessage);
+			/* ==========================================================*/            
+			
+			/* ==========================================================*/
+			/* This block on code assign custom object field name to internal 
+			QlikSense field names. For exampleL $Field contains all the 
+			field names. */
 
+			var myField = app.field("'" + myObjLockUnlockProps.fieldname + "'");
+			myField.getData();
+			/* ==========================================================*/
+
+			/* ==========================================================*/
+			/* This code LOCKS and UNLOCKS fields based on user selections */
+			
+			var myObjSelectedLockedFields = {};
+
+			/* ==========================================================*/
+			/* ==========================================================*/
 			$("#id_lock_unlock_toggle").click(function myFuncLockUnLockToggle(){
-				if(myValueToggle % 2 === 0){
-					app.lockAll();
-					myValueToggle = 1;
-				} else{ 
-					app.unlockAll();
-					myValueToggle = 0;
-				}
-			});
+				myObjSelectedLockedFields = {};
+				
+				/* ----------------------------------------------------------*/
+				myField.rows.forEach(function (item, index){
+					myObjSelectedLockedFields[item.qText] = item.qState;			  						  		
+			  	});
+				/* ----------------------------------------------------------*/
 
+				/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+				if( $('#id_lock_unlock_toggle').is(':checked') ){
+					
+					if(myObjLockUnlockProps.option === 1){
+						app.lockAll();
+					} else if(myObjLockUnlockProps.option === 2){
+						for (var myProps in myObjSelectedLockedFields){
+							if (myObjSelectedLockedFields[myProps] === 'S'){
+								app.field(myProps).lock();
+							}
+						}											
+					}
+
+				} else if( !$('#id_lock_unlock_toggle').is(':checked') ){
+					
+					/* ......................................................*/
+					if(myObjLockUnlockProps.option === 1){
+						app.unlockAll();
+						if(myObjLockUnlockProps.clearfield){
+							app.clearAll();
+						}
+					} else if(myObjLockUnlockProps.option === 2){
+						for (var myProps in myObjSelectedLockedFields){
+							if (myObjSelectedLockedFields[myProps] === 'S'){
+								app.field(myProps).unlock();
+								if(myObjLockUnlockProps.clearfield){
+									app.field(myProps).clear()
+								}
+							}
+						}						
+					}
+					/* ......................................................*/
+				}
+				/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+			});
+			/* ==========================================================*/
+			/* ==========================================================*/
 		}
 	};
 
 } );
+
+
+
